750ee111658e288d8e2fbc30cbb5e552a1bed5d3
solosolius <soliusaltijddemijne@gmail.com>
2022-09-04 11:46:48 +0800

    Moved most of factory's program to an abstract class and made it a singleton

--- src/main/java/com/example/city_clicker/building.java
+++ src/main/java/com/example/city_clicker/building.java
@@ -1,14 +1,57 @@
 package com.example.city_clicker;
 
-public interface building {
-    public int returnCurrentLevel();
-    public int returnUpgradeCost();
-    public int returnIncome();
-    public boolean returnPurchased();
-    public void levelDown();
-    public void levelUp();
-    private int calculateUpgradeCost(boolean up) {
-        return 0;
+public abstract class building {
+    private static boolean purchased;
+    private static int upgradeCost; //shows cost for the next level
+    private final int purchaseCost = 0;
+    private static int level = 1;
+    private static int incomePerSecond;
+
+
+    public building() {
+        purchased = false;
     }
-    public void setPurchased(boolean p); //For refunding/buying the property
+
+    protected int calculateUpgradeCost(boolean up) {
+        //upgradecost = upgradecost*1.2 -> for the next level
+        //upgradecost = upgradcost*0.8
+        if(up) {
+            return (int) (upgradeCost * 1.2);
+        }
+        //If level is decreasing
+        return (int) (upgradeCost/1.2);
+    }
+
+    public void levelDown() {
+        level--;
+        upgradeCost = calculateUpgradeCost(false);
+        incomePerSecond = level;
+    }
+
+    public void levelUp() {
+        level++;
+        upgradeCost = calculateUpgradeCost(true); //display upgrade cost for next level
+        incomePerSecond = level;
+    }
+    public int returnCurrentLevel() {
+        return level;
+    }
+
+    public int returnUpgradeCost() {
+        return upgradeCost;
+    }
+
+    public int returnIncome() {
+        return incomePerSecond;
+    }
+
+    public boolean returnPurchased() {
+        return purchased;
+    }
+
+
+    public void setPurchased(boolean p) {
+        purchased = p;
+    }
+
 }
--- src/main/java/com/example/city_clicker/factory.java
+++ src/main/java/com/example/city_clicker/factory.java
@@ -1,65 +1,28 @@
 package com.example.city_clicker;
 
-public class factory implements building{
+public class factory extends building{
 
     private static boolean purchased;
     private static int upgradeCost; //shows cost for the next level
     private final int purchaseCost = 10;
     private static int level = 1;
     private static int incomePerSecond;
+    private static factory instance = null;
 
-    public factory() {
-        purchased = false;
-        upgradeCost = (int)(purchaseCost*1.2);
+
+    //Private constructor
+    private factory() {
+        super();
         incomePerSecond = 1;
+        upgradeCost = (int)(purchaseCost*1.2);
     }
 
-    private int calculateUpgradeCost(boolean up) {
-        //upgradecost = upgradecost*1.2 -> for the next level
-        //upgradecost = upgradcost*0.8
-        if(up) {
-            return (int) (upgradeCost * 1.2);
-        }
-        //If level is decreasing
-        return (int) (upgradeCost/1.2);
+    //Static method to get the instance of factory.
+    public static factory getInstance() {
+        if (instance == null)
+            instance = new factory();
+        return instance;
     }
 
-    @Override
-    public void levelDown() {
-        level--;
-        upgradeCost = calculateUpgradeCost(false);
-        incomePerSecond = level;
-    }
 
-    @Override
-    public void levelUp() {
-        level++;
-        upgradeCost = calculateUpgradeCost(true); //display upgrade cost for next level
-        incomePerSecond = level;
-    }
-
-    @Override
-    public int returnCurrentLevel() {
-        return level;
-    }
-
-    @Override
-    public int returnUpgradeCost() {
-        return upgradeCost;
-    }
-
-    @Override
-    public int returnIncome() {
-        return incomePerSecond;
-    }
-
-    @Override
-    public boolean returnPurchased() {
-        return purchased;
-    }
-
-    @Override
-    public void setPurchased(boolean p) {
-        purchased = p;
-    }
 }
